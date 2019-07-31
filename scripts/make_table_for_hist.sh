2318b703ad6babba2541460e97f83a2102c285f3
ConYel <yeles.konstantinos@gmail.com>
2019-07-31 15:30:04 +0300

    upadate make_table_for_hist.sh

--- scripts/make_table_for_hist.sh
+++ scripts/make_table_for_hist.sh
@@ -4,7 +4,7 @@
 ## make a table for histogram from soft clipped alignment file Aligned.out.filtered.sorted.bam 
 
 INPUT_F="$1"
-nthreads="$2:-4"
+nthreads="${2:-4}"
 
 if [ $# -lt 1 ]; then
   echo "USAGE: <INPUT_FOLDER> folder with SPAR results from different samples <nthreads>"
@@ -15,11 +15,10 @@ fi
 for filt_bam in $INPUT_F/*/mapping/Aligned.out.filtered.sorted.bam; do
 [ -f "$filt_bam" ] || continue
 #  str="${filt_bam##$INPUT_F/}"      #remove guiding path
-  filename="${str%%/mapping}" #remove trailing path
+  filename="${filt_bam%%/mapping*}" #remove trailing path
 #  figur="${str##*/}"           #remove guiding
 #  figure="${figur%%.png}"      #get figure name
- echo "$filt_bam " 
-samtools view "$filt_bam" -@ $nthreads  | awk '{print length($10)}'  | sort -u > ${filename}/results/histogram_table.txt
- done
-
-## Konstantinos Con_Yel
+ echo "$filt_bam" 
+ echo "$filename"
+ echo "samtools view -@ $nthreads  "$filt_bam"   | awk '{print length($10)}'  | sort -u > ${filename}/results/histogram_table.txt"
+  done
